529732f491f7588fe9f1c84f3f37962a1c3604b0
hushuang <hushuang@yiguo.com>
2018-02-08 16:39:45 +0800

    admin

---
+++ html/js/admin.js
@@ -0,0 +1,91 @@
+﻿layui.define(['transition'], function (exports) {
+    var $ = layui.jquery,
+        l = layui.device(),
+        w = $(window),
+        b = $('body'),
+    m = {
+
+    }, e = {
+        navExpand: function (tree, parentLi) {
+            if (options.accordion) {
+                var openMenuLi = parentLi.siblings(selector.open);
+                var openTree = openMenuLi.children(selector.dropdownMenu);
+                e.navCollapse(openTree, openMenuLi);
+            }
+
+            var complete = function () {
+                tree.removeClass(className.transitioning).height('auto');
+            }
+
+            parentLi.addClass(className.open);
+            tree.addClass(className.transitioning).height(0).one("bsTransitionEnd", $.proxy(complete, this)).emulateTransitionEnd(options.animationSpeed).height(tree[0].scrollHeight);
+        },
+        navCollapse: function (tree, parentLi) {
+            var complete = function () {
+                parentLi.removeClass(className.open);
+                tree.removeClass(className.transitioning).height('');
+                tree.find(selector.open).removeClass(className.open);
+            }
+
+            if (tree && tree.length) {
+                tree.height(tree.height())[0].offsetHeight;
+                tree.addClass(className.transitioning).height(0).one("bsTransitionEnd", $.proxy(complete, this)).emulateTransitionEnd(options.animationSpeed);
+            }
+        }
+    }, options = {
+        animationSpeed: 350,
+        accordion: true,
+        followLink: false
+    }, selector = {
+        dropdownMenu: '.px-nav-dropdown-menu',
+        active: '.active',
+        open: '.open',
+        dropdown: '.px-nav-dropdown',
+        trigger: '.px-nav-item > a',
+        navToggle: '.px-nav-toggle',
+        navCollapse: '.px-nav-collapse',
+        navExpand: '.px-nav-expand',
+        pxNav: '.px-nav'
+    }, className = {
+        open: 'open',
+        transitioning: 'px-nav-transitioning',
+        pxNav: 'px-nav',
+        navCollapse: 'px-nav-collapse',
+        navExpand: 'px-nav-expand'
+    };
+
+    b.on('click', selector.trigger, function () {
+        var _this = $(this),
+            dropdownMenu = _this.next(selector.dropdownMenu),
+            parentLi = _this.parent(),
+        isOpen = parentLi.hasClass(className.open);
+
+        if (!parentLi.is(selector.dropdown)) {
+            return;
+        }
+
+        if (isOpen) {
+            e.navCollapse(dropdownMenu, parentLi);
+        } else {
+            e.navExpand(dropdownMenu, parentLi);
+        }
+    }).on('click', selector.navToggle, function () {
+        var pxNav = $(selector.pxNav);
+        if (pxNav.attr('class') === className.pxNav) {
+            var expand = w.width() > 992;
+            if (expand) {
+                pxNav.addClass(className.navCollapse);
+            } else {
+                pxNav.addClass(className.navExpand);
+            }
+        } else {
+            if (pxNav.hasClass(className.navCollapse)) {
+                pxNav.removeClass(className.navCollapse).addClass(className.navExpand);
+            } else {
+                pxNav.removeClass(className.navExpand).addClass(className.navCollapse);
+            }
+        }
+    });
+
+    exports('admin', m);
+});
---
+++ html/js/index.js
@@ -0,0 +1,51 @@
+﻿; layui.define(['admin', 'table'], function (e) {
+    var table = layui.table,
+        data = [
+            {
+                "id": 10000,
+                "username": "user-0",
+                "sex": "女",
+                "city": "城市-0",
+                "sign": "签名-0",
+                "experience": 255,
+                "logins": 24,
+                "wealth": 82830700,
+                "classify": "作家",
+                "score": 57
+            },
+          {
+              "id": 10001,
+              "username": "user-1",
+              "sex": "男",
+              "city": "城市-1",
+              "sign": "签名-1",
+              "experience": 884,
+              "logins": 58,
+              "wealth": 64928690,
+              "classify": "词人",
+              "score": 27
+          }
+        ];
+
+    table.render({
+        elem: '#table'
+        , height: 315
+        , data: data
+        , page: true
+        , even:true
+        , cols: [[
+            { type: 'numbers' }
+      , { type: 'checkbox' }
+          , { field: 'username', title: '用户名', width: 80 }
+          , { field: 'sex', title: '性别', width: 80, sort: true }
+          , { field: 'city', title: '城市', width: 80 }
+          , { field: 'sign', title: '签名', width: 177 }
+          , { field: 'experience', title: '积分', width: 80, sort: true }
+          , { field: 'score', title: '评分', width: 80, sort: true }
+          , { field: 'classify', title: '职业', width: 80 }
+          , { field: 'wealth', title: '财富', width: 135, sort: true }
+        ]]
+    });
+
+    e('index', {})
+});
